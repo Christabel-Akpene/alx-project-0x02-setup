6e36b2a160239d1224cabe31fc9f2faaf4168c8d
Christabel-Akpene <dc.akpene@gmail.com>
2025-11-21 13:58:53 +0000

    setup

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+      <div className="min-h-dvh flex items-center justify-center">
+          <h1 className="text-2xl">Welcome, Home</h1>
+      </div>
+  );
+}
